6cd2b8fc02b1acc25a7d4316881bfdc84d830e7d
Itay Levy <itaylevy134@gmail.com>
2020-03-11 02:21:15 +0200

    created read_primes function

--- solution.py
+++ solution.py
@@ -1,8 +1,15 @@
 import sympy
+import itertools
+
+
+def read_primes():
+    with open('primes_list.txt', 'r') as file:
+        return list(itertools.chain.from_iterable([map(int, line.split()) for line in file]))
 
 
 def main():
-    pass
+    primes = read_primes()
+    print(primes)
 
 
 if __name__ == '__main__':

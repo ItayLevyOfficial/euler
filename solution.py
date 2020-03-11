c93c96f55df088f68d314744f77daf43e1154f26
Itay Levy <itaylevy134@gmail.com>
2020-03-11 03:07:22 +0200

    works !!!

--- solution.py
+++ solution.py
@@ -28,9 +28,19 @@ def main():
                 for prime3_index in range(prime2_index, len(primes)):
                     prime3 = primes[prime3_index]
                     if prime3 in sets_map[prime2_index] and prime3 in sets_map[prime1_index]:
-                        # create_pairs_if_needed(prime_index=prime3_index, primes=primes, sets_map=sets_map)
-                        print(primes[prime1_index], primes[prime2_index], primes[prime3_index])
-                        return
+                        create_pairs_if_needed(prime_index=prime3_index, primes=primes, sets_map=sets_map)
+                        for prime4_index in range(prime3_index, len(primes)):
+                            prime4 = primes[prime4_index]
+                            if prime4 in sets_map[prime2_index] and prime4 in sets_map[prime1_index] and prime4 in sets_map[prime3_index]:
+                                create_pairs_if_needed(prime_index=prime4_index, primes=primes, sets_map=sets_map)
+                                for prime5_index in range(prime4_index, len(primes)):
+                                    prime5 = primes[prime5_index]
+                                    if prime5 in sets_map[prime2_index] and prime5 in sets_map[
+                                        prime1_index] and prime5 in sets_map[prime3_index] and prime5 in sets_map[prime4_index]:
+                                        create_pairs_if_needed(prime_index=prime5_index, primes=primes,
+                                                               sets_map=sets_map)
+                                        print(primes[prime1_index], primes[prime2_index], prime3, prime4, prime5)
+
 
 def create_pairs_if_needed(prime_index, primes, sets_map):
     if prime_index not in sets_map:

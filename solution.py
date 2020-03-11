5134d2b96d82380dae3831c0c105d824ee843d32
Itay Levy <itaylevy134@gmail.com>
2020-03-11 02:33:22 +0200

    did some shit

--- solution.py
+++ solution.py
@@ -2,14 +2,31 @@ import sympy
 import itertools
 
 
-def read_primes():
+def read_primes() -> [int]:
     with open('primes_list.txt', 'r') as file:
         return list(itertools.chain.from_iterable([map(int, line.split()) for line in file]))
 
 
+def create_primes_set(prime: int, primes: [int]) -> {int}:
+    primes_set = set()
+    for prime2 in primes:
+        if sympy.isprime(int(f'{prime}{prime2}')) and sympy.isprime(int(f'{prime2}{prime}')):
+            primes_set.add(prime2)
+    return primes_set
+
+
 def main():
     primes = read_primes()
-    print(primes)
+    sets_map = {}
+    for prime1 in primes:
+        create_pairs_if_needed(prime1, primes, sets_map)
+        for prime2 in primes:
+            if prime2 != prime1 and prime2 in sets_map[prime1]:
+
+
+def create_pairs_if_needed(prime, primes, sets_map):
+    if prime not in sets_map:
+        sets_map[prime] = create_primes_set(prime=prime, primes=primes)
 
 
 if __name__ == '__main__':
